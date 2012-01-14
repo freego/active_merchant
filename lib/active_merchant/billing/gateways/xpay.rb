a65b86791261aaddd299934f0b33d0aa81f846d9
Alessandro Lepore <a.lepore@freegoweb.it>
2012-01-14 17:41:25 +0100

    gateway base with some unit tests

--- lib/active_merchant/billing/gateways/xpay.rb
+++ lib/active_merchant/billing/gateways/xpay.rb
@@ -1,20 +1,26 @@
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class XpayGateway < Gateway
-      TEST_URL = 'https://example.com/test'
-      LIVE_URL = 'https://www.x-pay.it/XPServlet'
-      TERMINAL_ID = "" # 16 chars
+      
+      # -> VPOSReqLight
+      # ( <- VPOSResLight (KO) )
+      # <- VPOSNotification (OK)
+      # -> VPOSNotificationRes
+      
+      # static fields
+      TEST_URL = 'https://www.x-pay.it/XPServlet' # test url
+      LIVE_URL = 'https://www.x-pay.it/XPServlet' # test url
+      TERMINAL_ID = "0000000050666666" # 16 chars # test
       ACTION_CODE = 'AUT' # AUT or AUT-CONT
       LANGUAGE = 'ITA'
-      NOTIFICATION_URL = 'http://url'
-      RESULT_URL = 'http://url'
-      ANNULMENT_URL = 'cac'
-      ERROR_URL = 'cac'
+      NOTIFICATION_URL = 'http://url.not'
+      RESULT_URL = 'http://url.res'
+      ANNULMENT_URL = 'http://url.ann'
+      ERROR_URL = 'http://url.err'
       VERSION_CODE = '01.00'
       CO_PLATFORM = 'L'
       MESSAGE_TYPE = 'C00'
-      MAC_KEY = ''
-      
+      MAC_KEY = '6664F1F621A5DED95C7EE8C5507A9E1F2970BCFE' # test
       
       # The countries the gateway supports merchants from as 2 digit ISO country codes
       self.supported_countries = ['IT']
@@ -26,12 +32,19 @@ module ActiveMerchant #:nodoc:
       self.homepage_url = 'http://www.cartasi.it/gtwpages/common/index.jsp?id=OiRGdkfJWU'
       
       # The name of the gateway
-      self.display_name = 'X-Pay'
+      self.display_name = 'Cartasi X-Pay'
+      self.application_id = 'xpay'
       
       self.money_format = :cents
       
       CURRENCY_CODES = {
         'EUR' => '978',
+        'AUD' => '036',
+        'CAD' => '124',
+        'HKD' => '344',
+        'JPY' => '392',
+        'CHF' => '756',
+        'GBP' => '826',
         'USD' => '840'
       }
       
@@ -40,44 +53,37 @@ module ActiveMerchant #:nodoc:
       ########################################################################################
       
       def initialize(options = {})
-        #requires!(options, :login, :password)
         @options = options
         super
       end  
       
-      def authorize(money, creditcard, options = {})
+      # VPOSReqLight
+      def purchase(money, options = {})
         post = {}
-        
-        # add_invoice(post, options)
-        # add_creditcard(post, creditcard)        
-        # add_address(post, creditcard, options)        
-        # add_customer_data(post, options)
-        
-        commit('authonly', money, post)
+        build_post(post, money, options)
+        commit('sale', post)
+      end                       
+    
+      # VPOSNotification
+      def confirm
+        post = {}
+        post['RESPONSE'] = 0
+        commit('confirm', post)
       end
+    
+      private                       
       
-      def purchase(money, creditcard, options = {})
-        post = {}
-        
+      def build_post(post, money, options)
         add_static_fields(post)
         add_transaction_id(post, options)
         add_amount(post, money)
-        add_currency(post)
+        add_currency(post, options)
         add_language(post)
         add_email(post, options) # facultative
         add_desc_order(post, options) # facultative
         # add_optional_fields(post, options) # facultative
         add_mac(post)
-                
-        commit('sale', post)
-      end                       
-    
-      def capture(money, authorization, options = {})
-        commit('capture', money, post)
       end
-    
-    
-      private                       
       
       def add_static_fields(post)
         post['TERMINAL_ID'] = TERMINAL_ID
@@ -92,7 +98,7 @@ module ActiveMerchant #:nodoc:
       end
       
       def add_transaction_id(post, options)
-        post['TRANSACTION_ID'] = options[:order_id] 
+        post['TRANSACTION_ID'] = "%020d" % options[:order_id] 
       end
       
       def add_amount(post, money)
@@ -101,9 +107,17 @@ module ActiveMerchant #:nodoc:
         post['AMOUNT'] = money
       end
       
-      def add_currency(post)
-        # TODO dynamic?
-        post['CURRENCY'] = default_currency
+      def add_currency(post, options)
+        # xpay numeric value
+        if Numeric === options[:currency]
+          post['CURRENCY'] = options[:currency]
+        # or 3 char code
+        else
+          post['CURRENCY'] = CURRENCY_CODES[options[:currency]]
+        # TODO fallback
+        # else
+          # post['CURRENCY'] = self.default_currency
+        end
       end
       
       def add_language(post)
@@ -124,38 +138,43 @@ module ActiveMerchant #:nodoc:
       end
       
       def add_mac(post)
-        # TODO
-        mac = Digest::SHA1.hexdigest("#{TERMINAL_ID}#{TRANSACTION_ID}#{post['AMOUNT']}#{post['CURRENCY']}#{VERSION_CODE}#{CO_PLATFORM}#{ACTION_CODE}#{post['EMAIL']}#{MAC_KEY}")
-        post['MAC'] = mac
+        string = ''
+        string << "#{TERMINAL_ID}"
+        string << "#{post['TRANSACTION_ID']}"
+        string << "#{post['AMOUNT']}"
+        string << "#{post['CURRENCY']}"
+        string << "#{VERSION_CODE}"
+        string << "#{CO_PLATFORM}"
+        string << "#{ACTION_CODE}"
+        string << "#{post['EMAIL']}"
+        string << "#{MAC_KEY}"
+        
+        post['MAC'] = Digest::SHA1.hexdigest( string )
       end
       
-      # def add_customer_data(post, options)
-      # end
-# 
-      # def add_address(post, creditcard, options)      
-      # end
-# 
-      # def add_invoice(post, options)
-      # end
-#       
-      # def add_creditcard(post, creditcard)      
-      # end
-#       
-      # def parse(body)
-      # end     
-#       
-      def commit(action, post)
-        request = post.collect { |key, value| "{key}=#{CGI.escape(value.to_s)}" }.join("&")
-        puts request
-        response = ssl_post((test? ? TEST_URL : LIVE_URL), request )
-        puts response
+      def parse(body)
+        Rack::Utils.parse_query(body)
       end
-# 
-      # def message_from(response)
-      # end
-#       
-      # def post_data(action, parameters = {})
-      # end
+      
+      def post_data(post)
+        post.collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
+      end
+       
+      def commit(action, post)
+        request = post_data(post)
+        # puts 'Request:'
+        # puts request.to_yaml
+        response = parse( ssl_post((test? ? TEST_URL : LIVE_URL), request ) )
+        # puts 'Response:'
+        # puts response
+        case action
+          when 'sale'
+          Response.new(response['RESPONSE'] == 'TRANSACTION_OK', response['RESPONSE'], response,
+            :test => @options[:test] || test?,
+          )
+        end
+      end
+      
     end
   end
 end
--- test/unit/gateways/xpay_test.rb
+++ test/unit/gateways/xpay_test.rb
@@ -2,50 +2,92 @@ require 'test_helper'
 
 class XpayTest < Test::Unit::TestCase
   def setup
-    @gateway = XpayGateway.new(
-                 # :login => 'login',
-                 # :password => 'password'
-               )
+    @gateway = XpayGateway.new()
 
     @credit_card = credit_card
-    @amount = 100.11
+    @amount = 666.66
     
     @options = { 
       :order_id => '1',
       :billing_address => address,
       :description => 'Store Purchase',
       :email => 'prova@freego.it',
+      :currency => "EUR",
       :test => true
     }
   end
   
-  def test_successful_purchase
+  def test_successful_request
     @gateway.expects(:ssl_post).returns(successful_purchase_response)
     
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_instance_of 
+    assert response = @gateway.purchase(@amount, @options)
+    # puts response.to_yaml
+    assert_instance_of XpayGateway, @gateway
     assert_success response
-    
-    # Replace with authorization number from the successful response
-    assert_equal '', response.authorization
+    # assert_equal response.params['MAC'], @gateway.send(:add_mac, {})
     assert response.test?
   end
 
   def test_unsuccessful_request
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
     
-    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert response = @gateway.purchase(@amount, @options)
+    
+    assert_instance_of XpayGateway, @gateway
     assert_failure response
     assert response.test?
+  end
+
+  def test_post_data
+    post = {}
+    @gateway.send(:build_post, post, @amount, @options)
+    assert_equal 16, post['TERMINAL_ID'].length, 'not 16 chars'
+    assert_equal 20, post['TRANSACTION_ID'].length, 'not20 chars'
+    assert post['ACTION_CODE'].length < 9, 'more than 8 chars'
+    assert_equal 9, post['AMOUNT'].length, 'not 20 chars'
+    assert !(post['AMOUNT'].include? "."), 'contains dots'
+    assert !(post['AMOUNT'].include? ","), 'contains commas'
+    assert_equal 3, post['CURRENCY'].length, 'not 3 chars'
+    assert post['NOTIFICATION_URL'].length < 260, 'more than 260 chars'
+    assert post['RESULT_URL'].length < 260, 'more than 260 chars'
+    assert post['ERROR_URL'].length < 260, 'more than 260 chars'
+    assert post['ANNULMENT_URL'].length < 260, 'more than 260 chars'
+    assert_equal '01.00', post['VERSION_CODE'], 'not developed for this version'
+    assert post['EMAIL'].length < 100, 'more than 100 chars'
+    assert_match /^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/, post['EMAIL'], 'invalid email address'
+    assert_equal 1, post['CO_PLATFORM'].length, 'not 1 char'
+    assert_equal 40, post['MAC'].length, 'not 40 chars'
   end
 
   private
   
   # Place raw successful response from gateway here
   def successful_purchase_response
+    success_string = common_purchase_response
+    success_string << "RESPONSE=TRANSACTION_OK"
   end
   
   # Place raw failed response from gateway here
-  def failed_purcahse_response
+  def failed_purchase_response
+    fail_string = common_purchase_response
+    fail_string << "RESPONSE=TRANSACTION_KO"
   end
+ 
+   def common_purchase_response
+    string = ''
+    string << "TERMINAL_ID=#{@gateway.class::TERMINAL_ID}&"
+    string << "TRANSACTION_ID=#{@options[:order_id]}&"
+    string << "AUTH_CODE=901867&"
+    string << "TRANSACTION_DATE=01/01/2012 16.55.56&"
+    string << "CARD_TYPE=VISA&"
+    string << "AMOUNT=#{@amount}&"
+    string << "CURRENCY=#{@options[:currency]}&"
+    string << "TRANSACTION_TYPE=NO_3DSECURE&"
+    string << "MAC=b889211bb7f57b753419d78e75f08762a46fdd10&"
+    string << "REGION=Europe&"
+    string << "COUNTRY=Italy&"
+    string << "PRODUCT_TYPE=Consumer&"
+    string << "LIABILITY_ SHIFT=N&"
+  end
+  
 end
